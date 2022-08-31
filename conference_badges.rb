443e111ecd7f088d7c7b9f73d278f61bf7e6141c
Win Feigle <win.feigle@gmail.com>
2022-08-30 23:03:39 -0600

    commit tree 95de3d26019da33a17869ac69b881f9a0b7e8a7e

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,25 @@
 # Write your code here.
+
+def badge_maker attendee
+    "Hello, my name is #{attendee}."
+end
+
+def batch_badge_creator attendees
+    attendees.map{ |name| "Hello, my name is #{name}." }
+end
+
+def assign_rooms attendees
+    attendees.map.with_index(1) do |attendee, room_number|
+        "Hello, #{attendee}! You'll be assigned to room #{room_number}!"
+    end
+end
+
+def printer attendees
+    batch_badge_creator(attendees).each do |badge|
+        puts badge
+    end
+
+    assign_rooms(attendees).each do |assignment|
+        puts assignment
+    end
+end
